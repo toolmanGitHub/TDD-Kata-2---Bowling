1984f94c69b76ef8eae09930d1fa5bfae96fb5bc
Tim Taylor <timthetoolmantaylor@gmail.com>
2012-09-01 19:26:43 -0700

    Initial Commit

---
+++ Kata 2 Bowling/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Kata 2 Bowling
+//
+//  Created by Tim Taylor on 9/1/12.
+//  Copyright (c) 2012 Tim Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
