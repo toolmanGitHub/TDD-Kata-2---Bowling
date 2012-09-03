8a8691bd24a96e56358db80bfc0f9465d6ce45e2
Tim Taylor <timthetoolmantaylor@gmail.com>
2012-09-02 23:36:58 -0700

    Updated tests and logic

--- Bowling Game Score/BDBowlingGameScoreTests.m
+++ Bowling Game Score/BDBowlingGameScoreTests.m
@@ -45,10 +45,104 @@
     STAssertEquals(expectedScore, actualScore, @"BDBowlingGame single pin per roll should be scored correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
 }
 
+
+
+-(void)testThatClassScoresASpareCorrectly_One{
+    [self rollSpareWithFirstRoll:5 secondRoll:5];
+    
+    [bowlingGame roll: 3]; // First ball of next frame is bonus for the spare in the previous frame!
+    
+    [self rollManyTimes:17 pins:0];
+    NSInteger expectedScore=(5+5+3 +3);
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame bowling a spare should score correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+    
+}
+
+-(void)testThatClassScoresASpareCorrectly_Two{
+    [self bowlFrameWithFirstRoll:5 secondRoll:4];
+    [self rollSpareWithFirstRoll:3 secondRoll:7];
+    [bowlingGame roll: 2];
+    
+    [self rollManyTimes:15 pins:0];
+    NSInteger expectedScore=(5+4+3+7+2 +2);
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame bowling a spare should score correctly, test two.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+    
+}
+
+-(void)testThatClassScoresAStrikeCorrectly{
+    [bowlingGame roll: 10];  // A strike
+    [self bowlFrameWithFirstRoll:5 secondRoll:2];
+    [self rollManyTimes:15  pins:0];
+    
+    NSInteger expectedScore=(10+5+2 +5+2);
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame bowling a strike should score correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+}
+
+-(void)testThatClassScoresAPerfectGameCorrectly{
+    [self rollManyTimes:12 pins:10];
+    NSInteger expectedScore=300;
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame bowling a perfect game should score correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+}
+
+-(void)testThatClassScoresANotQuitePerfectGame{
+    [self rollManyTimes:10 pins:10];
+    
+    [self bowlFrameWithFirstRoll:5 secondRoll:5];
+    NSInteger expectedScore=285;
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame bowling a perfect game should score correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+}
+
+-(void)testThatClassScoresANotQuitePerfectGame_Two{
+    [self rollManyTimes:9 pins:10];
+    
+    [bowlingGame roll:5];
+    [bowlingGame roll:5]; // Spare in Final Frame
+    [bowlingGame roll:10]; // Strike for third roll of final frame
+    NSInteger expectedScore=275;
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame bowling a perfect game should score correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+}
+
+-(void)testThatClassScoresUncleBobsCorrectly{
+    [self bowlFrameWithFirstRoll:1 secondRoll:4];
+    [self bowlFrameWithFirstRoll:4 secondRoll:5];
+    [self rollSpareWithFirstRoll:6 secondRoll:4];
+    [self rollSpareWithFirstRoll:5 secondRoll:5];
+    [bowlingGame roll:10]; // strike in fifth frame
+    [self bowlFrameWithFirstRoll:0 secondRoll:1];
+    [self bowlFrameWithFirstRoll:7 secondRoll:3];
+    [self bowlFrameWithFirstRoll:6 secondRoll:4];
+    [bowlingGame roll:10];  // strike in ninth frame
+    [self rollSpareWithFirstRoll:2 secondRoll:8];
+    [bowlingGame roll:6];
+    
+    NSInteger expectedScore=133;
+    NSInteger actualScore=bowlingGame.score;
+    STAssertEquals(expectedScore, actualScore, @"BDBowlingGame Uncle Bob's game should score correctly.  Expected:  %ld.  Got:  %ld",expectedScore,actualScore);
+}
+
+#pragma mark -
+#pragma mark Helpers
+
+-(void)bowlFrameWithFirstRoll:(NSInteger)rollOne secondRoll:(NSInteger)rollTwo{
+    [bowlingGame roll:rollOne];
+    [bowlingGame roll:rollTwo];
+}
+
 -(void)rollManyTimes:(NSInteger)rolls pins:(NSInteger)pins{
     for (NSInteger iCntr=0; iCntr<rolls; iCntr++) {
         [bowlingGame roll:pins];
     }
+}
+
+-(void)rollSpareWithFirstRoll:(NSInteger)rollOnePins secondRoll:(NSInteger)rollTwoPins{
+    [bowlingGame roll: rollOnePins];
+    [bowlingGame roll: rollTwoPins];
 }
 
 @end
--- Kata 2 Bowling/Classes/BDBowlingGame.m
+++ Kata 2 Bowling/Classes/BDBowlingGame.m
@@ -20,11 +20,56 @@
 
 -(NSInteger)score{
     sum=0;
-    for (NSInteger iCntr=0; iCntr<=currentRoll; iCntr++) {
-        sum+=rolls[iCntr];
+    NSInteger currentFrame=0;
+    NSInteger roll=0;
+    for (currentFrame=0 ; currentFrame<10; currentFrame++) {
+        if ([self isStrikeForRoll:roll]) {
+            sum+=10+[self strikeBonusForRoll:roll];
+            roll++;
+        }else{
+            if ([self isSpareForRoll:roll]) {
+                NSLog(@"  isSpare");
+                sum+=10l+[self spareBonusForRoll:roll];
+            }else{
+                sum+=rolls[roll]+rolls[roll+1];
+            }
+            
+            roll+=2;
+        }
+        NSLog(@"frame:  %ld, score=%ld",currentFrame,sum);
+        
     }
     
     return sum;
+}
+
+-(BOOL)isSpareForRoll:(NSInteger)roll{
+    NSInteger pinsRollOne=rolls[roll];
+    NSInteger pinsRollTwo=rolls[roll+1];
+    
+    if (pinsRollOne+pinsRollTwo==10)
+        return YES;
+    return NO;
+}
+
+-(NSInteger)spareBonusForRoll:(NSInteger)roll{
+    NSLog(@"   spare bonus=%ld",rolls[roll+2]);
+    return rolls[roll+2];
+    
+}
+
+-(BOOL)isStrikeForRoll:(NSInteger)roll{
+
+    if (rolls[roll]==10) {
+        return YES;
+    }
+    return NO;
+}
+
+-(NSInteger)strikeBonusForRoll:(NSInteger)roll{
+    NSInteger nextRoll=roll+1;
+    return rolls[nextRoll]+rolls[nextRoll+1];
+    
 }
 
 
